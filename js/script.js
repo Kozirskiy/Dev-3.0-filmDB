1edcc89aa6245a8b68d1482d9285e9bc700c634a
AS <as1@i.ua>
2023-01-22 14:23:41 +0200

    44 less end

--- js/script.js
+++ js/script.js
@@ -24,14 +24,32 @@ const movieDB = {
     ]
 };
 
-const adv = document.querySelectorAll('.promo__adv img');
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
 
-// adv.forEach(item => {
-//     item.remove();
-// });
-adv.forEach( function(item) {
+adv.forEach(item => {
     item.remove();
 });
 
+
+genre.textContent = 'Drama';//1
+
+poster.style.backgroundImage = `url('img/bg.jpg')`;//2
+
+movieList.innerHTML = '';
+
+movieDB.movies.sort();
+
+console.log(poster.innerHTML);
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">${i + 1} ${film}
+        <div class="delete"></div>
+    </li>
+    `;
+}); 
 
 
